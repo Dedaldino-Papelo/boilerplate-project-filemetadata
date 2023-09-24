5b8c2b78ed9b1e2be083b0609c50c816c4dc1aa1
Dedalino <kelsonpapelo18@gmail.com>
2023-09-24 02:30:06 +0100

    import multer, create the route handler

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/uploads', express.static(process.cwd() + '/uploads'))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload('upfile'), (req, res, next) => {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    })
+    next()
+})
 
 
 const port = process.env.PORT || 3000;
