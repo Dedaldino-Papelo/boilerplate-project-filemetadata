6e09f2ad96d4fe1c32477374cb220a1499d7b452
Dedalino <kelsonpapelo18@gmail.com>
2023-09-24 02:32:44 +0100

    feate: create a middleware to look for uploads folder

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', upload('upfile'), (req, res, next) => {
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
     res.json({
       name: req.file.originalname,
       type: req.file.mimetype,
